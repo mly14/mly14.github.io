e5d5352ec0f80def9a408398814a24adec77c1c4
lymeg000 <lymeg000@uw.edu>
2023-02-07 11:12:46 -0800

    fixed sketch 6

--- mp2/sketches/sketch6/sketch6.js
+++ mp2/sketches/sketch6/sketch6.js
@@ -1,62 +1,46 @@
-let rings = [];
-let cnv;
 let osc, playing, freq, amp;
+let trail = [];
+let a = 0;
 
 function setup() {
-  cnv = createCanvas(400, 400);
+  let cnv = createCanvas(windowWidth, windowHeight);
   cnv.mousePressed(playOscillator);
   osc = new p5.Oscillator("sine");
 }
 
 function draw() {
-  cnv.mousePressed(playOscillator);
+  background(220);
+  freq = constrain(map(mouseY, 0, height, 500, 100), 100, 500);
 
-  background(255);
-
-  noFill();
-
-  strokeWeight(2);
-  if (frameCount % 2 === 0) {
-    rings.push(new Ring(mouseX, mouseY, 0));
-  }
-
-  rings.forEach((ring) => {
-    ring.draw();
-    ring.update();
-  });
-  soundfreq = constrain(map(mouseY, 0, height, 500, 100), 100, 500);
   let panning = map(mouseX, 0, width, -1.0, 1.0);
 
   if (playing) {
     // smooth the transitions by 0.1 seconds
-    osc.freq(soundfreq, 0.1);
+    osc.freq(freq, 0.1);
+    osc.amp(0.2);
     osc.pan(panning);
-    // osc.amp(amp, 0.1);
   }
-}
-
-function mousePressed() {
-  rings.push(new Ring(mouseX, mouseY, 0));
-}
-
-class Ring {
-  constructor(x, y, radius) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-  }
-  update() {
-    this.radius += 1;
-    if (this.radius > 50) {
-      rings.shift();
+  trail.push([mouseX, mouseY]);
+  for (let i = 0; i < trail.length; i++) {
+    noStroke();
+    fill("orange");
+    ellipse(trail[i][0], trail[i][1], 8);
+    // for (let i = 0; i < 10; i ++) {
+    //   ellipse(0, 30, 20, 80);
+    //   rotate(PI/5);
+    // }
+    if (a > 255) {
+      trail.shift();
+      a = 0;
     }
-  }
-  draw() {
-    circle(this.x, this.y, this.radius);
+    a += 8;
   }
 }
 
 function playOscillator() {
+  // starting an oscillator on a user gesture will enable audio
+  // in browsers that have a strict autoplay policy.
+  // See also: userStartAudio();
   osc.start();
   playing = true;
 }
@@ -64,5 +48,5 @@ function playOscillator() {
 function mouseReleased() {
   // ramp amplitude to 0 over 0.5 seconds
   osc.amp(0, 0.5);
-  // playing = false;
+  playing = false;
 }
